f6c97d816482e8fbe776eec0f576f7433f2a0c3c
dstguy <dstguy@users.noreply.github.com>
2023-01-14 00:22:47 +0200

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './logo.png';
+import React from 'react';
+import video from './video.mp4'
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='main'>
+      <video className='video' src={video} autoPlay muted={true} loop={true}></video>
+      <div className='container'>
+         <img className='logo' src={logo} alt="sign" />
+      </div>
     </div>
-  );
+  )
 }
-
-export default App;
